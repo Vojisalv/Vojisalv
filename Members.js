7495e5a83fe6d6cb9e5e745ec6c606dd1629e4aa
Vojisalv <97899843+Vojisalv@users.noreply.github.com>
2022-01-17 16:58:10 +0100

    Add files via upload

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+      id: 1,
+      name: 'Vojislav Milovanovic',
+      email: 'vojislavmilovanovic@gmail.com',
+      status: 'active'
+    },
+    {
+      id: 2,
+      name: 'Veljko Lukic',
+      email: 'veljkolukic@gmail.com',
+      status: 'inactive'
+    },
+    {
+      id: 3,
+      name: 'filipfilipovic',
+      email: 'filipfilipovic@gmail.com',
+      status: 'active'
+    }
+  ];
+  
+  module.exports = members;
